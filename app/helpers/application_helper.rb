376ef9c46311f962812fb79519ee28a753a985e6
Marcio <marcio@mail.com>
2017-08-12 22:34:42 -0300

    change extension image in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def destiny(id, total)
-    destination = Destination.find(id)
-    content_tag(:ul, class: 'collection') do
-      content_tag(:li, class: 'collection-item avatar') do
-        content_tag(:img, src: image_url("#{destination.name.parameterize.underscore}.jpg"), alt: destination.name, class: 'circle') <<
-        content_tag(:h5, destination.name) <<
-        content_tag(:p, truncate(destination.description, length: 100)) <<
-        content_tag(:a, content_tag(:span, pluralize(total, 'voto', 'votos'), class: 'badge'),  href: '#!', class: 'secondary-content') 
-      end
-    end
-  end  
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -3,7 +3,7 @@ module VotesHelper
     destination = Destination.find(id)
     content_tag(:ul, class: 'collection') do
       content_tag(:li, class: 'collection-item avatar') do
-        image_tag(destination.name.parameterize.underscore, class: 'circle') <<
+        image_tag("#{destination.name.parameterize.underscore}.jpg", class: 'circle') <<
         content_tag(:h5, destination.name) <<
         content_tag(:p, truncate(destination.description, length: 100)) <<
         content_tag(:a, content_tag(:span, pluralize(total, 'voto', 'votos'), class: 'badge'),  href: '#!', class: 'secondary-content') 
